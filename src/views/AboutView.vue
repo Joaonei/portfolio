aa71dfd8cb8baec0f488b4f6edc6b85488075cee
Joaonei <joaonfagundes26@gmail.com>
2023-08-09 15:32:58 -0400

    melorar layout

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,21 @@
 <template>
 
 <div class="
-grid grid-cols-1 md:grid-cols-2 
+flex flex-col
+items-center
 bg-white
  template 
- space-x-5 pb-80">
- <div class="w-1/2"> <img class="object-contain h-80 md:h-32 lg:h-80 w-120" src="../assets/myimage.png" alt=""></div>
- 
- <div class="w-1/2">
-    <p class="pl-5 ">
+ space-x-2 pb-80">
+ <div class="w-90 px-10"> <img class="object-contain h-80 md:h-32 lg:h-80 w-120" src="../assets/myimage.png" alt=""></div>
+ <div class="flex items-center">
+ <div class="w-30">
+    <p class="pl-5 text-center 
+    font-extrabold
+    bg-gray-100	opacity-75">
       <strong>RESUMO PROFISSIONAL</strong> Desenvolvedor Front End em transição de carreira com sólida experiência como Chef de cozinha, buscando ingressar no campo de Desenvolvimento Web. Atualmente, estou cursando Engenharia de Software na Wyden Educacional para aprimorar minhas habilidades em programação. Sou apaixonado por tecnologia e estou comprometido em combinar minha dedicação e meu amor pelo desenvolvimento para me tornar um Desenvolvedor Web competente.
     </p>
   </div>
-  
+</div>
 
 </div>
 
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -6,21 +6,23 @@
     mx-auto
     space-x-5 space-y-5">
         <div class="">
-            <a  href="https://api-user-kappa.vercel.app/#/users_view" target="_blank">
-            <img class="object-contain h-20 md:h-20 lg:h-1/5 w-30
+            <a 
+            href="https://api-user-kappa.vercel.app/#/users_view" target="_blank">
+            <img class="object-contain h-20 md:h-20 lg:h-1/2 w-30
             transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ..." src="../assets/Project/p1.png" alt="">
             </a>
-            <p>Gerenciador de Usuários utilizando a API do JsonPlaceHolder</p>
+            <p class="text-center">Gerenciador de Usuários utilizando a API do JsonPlaceHolder</p>
             <a href="https://github.com/Joaonei/Api_User" target="_blank">
                 <font-awesome-icon :icon="['fab', 'square-github']" size="2xl" />Repositório
             </a>
         </div>
 
         <div>
-            <a href="https://joaonei.github.io/table_graphs_fb-ads/" target="_blank">
-            <img class="object-contain h-20 md:h-20 lg:h-1/5 w-30
+            <a 
+            href="https://joaonei.github.io/table_graphs_fb-ads/" target="_blank">
+            <img class="object-contain h-20 md:h-20 lg:h-1/2 w-30
             transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ..." src="../assets/Project/p2.png" alt="">
-            <p>Gráfico integrado ao Graph API onde obtém dados reais de uma conta do Facebook ADS </p>
+            <p class="text-center">Gráfico integrado ao Graph API onde obtém dados reais de uma conta do Facebook ADS </p>
             
             <a href="https://github.com/Joaonei/table_graphs_fb-ads" target="_blank">
                 <font-awesome-icon :icon="['fab', 'square-github']" size="2xl" />Repositório
